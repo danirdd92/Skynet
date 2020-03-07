690bb15ad29d0b470a9fd2ab968246114c2d63be
Dani Rashba <danirdd92@gmail.com>
2020-03-07 11:42:09 +0200

    Merged CustomerInfo With Customer Model

--- Skynet.Domain/Models/Customer.cs
+++ Skynet.Domain/Models/Customer.cs
@@ -8,6 +8,10 @@ namespace Skynet.Domain.Models
         public string UserName { get; set; }
         public string Password { get; set; }
         public Guid UserRole { get; set; }
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CreditCard { get; set; }
     }
 }
