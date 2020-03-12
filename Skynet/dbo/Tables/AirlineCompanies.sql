be1985a83a765f6ee4205cd2fbba39be5a72c2c7
Dani <danirdd92@gmail.com>
2020-03-12 15:45:04 +0200

    Added Db to github

---
+++ Skynet/dbo/Tables/AirlineCompanies.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AirlineCompanies] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [AirlineName] NVARCHAR (128) NOT NULL,
+    [CountryId]   INT            NOT NULL,
+    CONSTRAINT [PK_AirlineCompanies] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_AirlineCompanies_Countries] FOREIGN KEY ([CountryId]) REFERENCES [dbo].[Countries] ([Id])
+);
+
---
+++ Skynet/dbo/Tables/Customers.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [Id]          INT              IDENTITY (1, 1) NOT NULL,
+    [UserName]    NVARCHAR (128)   NOT NULL,
+    [Password]    NVARCHAR (128)   NOT NULL,
+    [UserRole]    UNIQUEIDENTIFIER NOT NULL,
+    [FirstName]   NVARCHAR (128)   NOT NULL,
+    [LastName]    NVARCHAR (128)   NOT NULL,
+    [Address]     NVARCHAR (128)   NULL,
+    [PhoneNumber] NVARCHAR (128)   NULL,
+    [CreditCard]  NVARCHAR (128)   NULL,
+    CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
