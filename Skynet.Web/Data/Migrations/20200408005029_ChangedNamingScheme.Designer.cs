004151e9c8d81c50ac8dabb9c4dd623581d1ce6a
Dani <danirdd92@gmail.com>
2020-04-08 03:52:43 +0300

    Added Migration For Identity

--- Skynet.Web/Data/Migrations/20200408005029_ChangedNamingScheme.Designer.cs
+++ Skynet.Web/Data/Migrations/20200408005029_ChangedNamingScheme.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Skynet.Web.Data;
 
 namespace Skynet.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200408005029_ChangedNamingScheme")]
+    partial class ChangedNamingScheme
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
